9eb4d277e87184299a95a2023d6dbb9d2a813d22
akshh057 <akshh057@gmail.com>
2021-12-05 12:20:16 +0530

    Feat:new Ui and deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import Tempapp from './components/Tempapp';
-
+import './App.css'
 function App() {
   return (
     <div>
--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -1,74 +1,95 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import "./CSS/style.css";
-import AcUnitIcon from '@material-ui/icons/AcUnit';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
-import CloudIcon from '@material-ui/icons/Cloud';
+import CloudIcon from '@mui/icons-material/Cloud';
+import axios from 'axios';
 
 const Tempapp = () => {
-    // const [temp, setTemp] = useState(null);
 
-    const [city, setCity] = useState(null);
-    const [country, setCountry] = useState(null);
-    const [search, setSearch] = useState("Mumbai"); //112173060a86fb43967cc14bc0b6d40b
-    useEffect(() => {
-        const fetchApi = async () => {
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=112173060a86fb43967cc14bc0b6d40b`
-            const response = await fetch(url);
-            const resJson = await response.json();
-            setCity(
-                resJson.main
-            );
-            setCountry(
-                resJson.sys
-            )
+    const [data, setData] = useState({
+        city: {},
+        country: {},
+        weather: {},
+        name: '',
+        fetching: true
+    });
+    const [search, setSearch] = useState(""); //112173060a86fb43967cc14bc0b6d40b
 
-        }
-        fetchApi();
-    }, [search])
+    const fetchApi = async () => {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=112173060a86fb43967cc14bc0b6d40b`
+        const response = await axios.get(url);
+        response.data && setData((prevState) => {
+            return {
+                ...prevState,
+                city: response.data.main,
+                country: response.data.sys,
+                weather: response.data.weather[0],
+                name: response.data.name,
+                fetching: false
+            }
+        })
+
+
+    }
     const check = (e) => {
+        if (e.target.value.length === 0) {
+            setData((prevState) => {
+                return {
+                    ...prevState,
+                    city: {},
+                    country: {},
+                    name: '',
+                    fetching: true
+                }
+            })
+        }
         setSearch(
             e.target.value
         )
 
     }
+    console.log(data)
     return (
         <>
             <div className="main__div">
-                <h1>
-                    Weather App
-            </h1>
+                <h1 className="heading">
+                    Weather
+                </h1>
                 <div className="temp__div">
                     <div className="input__data">
-                        <input type="search" className="input__field"
-                            defaultValue=""
+                        <input type="text" className="input__field"
                             value={search}
                             onChange={check}
                         />
+                        <button onClick={search.length > 0 ? fetchApi : null} className="searchBtn">
+                            Search
+                        </button>
                     </div>
-                    {!city || !country ? (
-                        <p> Please Enter Correct City!</p>
+                    {data.fetching === true ? (
+                        <p > Please Enter City Name!</p>
                     ) : (
-                            <div className="temp__info">
-                                <div className="icons">
-                                    <WbSunnyIcon />
-                                    {/* <CloudIcon style={{ marginLeft: "10px" }} /> */}
-                                </div>
-                                <h1 className="temp__location">
-                                    <LocationOnOutlinedIcon /> {search}
-                                </h1>
-                                <h2 className="temp">
-                                    {city.temp}° Cel
-                                </h2>
-                                <h3>
-                                    Country: {country.country}
-                                </h3>
-                                <h4 className="tempmin__max">
-                                    Min :{city.temp_min} | Max : {city.temp_max}
-                                </h4>
+                        <div className="temp__info">
+                            <div className="icons">
+                                {/* <WbSunnyIcon /> */}
+                                {<img src={`http://openweathermap.org/img/wn/${data.weather.icon}@2x.png`} />}
                             </div>
+                            <h1 className="temp__location">
+                                <LocationOnOutlinedIcon /> {data.name}
+                            </h1>
+                            <h2 className="temp">
+                                {data.city.temp}° Cel
+                            </h2>
+                            <h3>
+                                Country: {data.country.country}
+                            </h3>
+                            <h4 className="tempmin__max">
+                                Min :{data.city.temp_min} | Max : {data.city.temp_max}
+                            </h4>
+                        </div>
 
-                        )}
+                    )}
+                    <footer> Made with ❤️ by Akshat</footer>
                 </div>
             </div>
         </>
