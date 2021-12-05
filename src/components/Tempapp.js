64ef03283e38aa5c2c07da4d888323819de361a2
akshh057 <akshh057@gmail.com>
2021-12-05 17:00:28 +0530

    refactor: api key to env

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -15,10 +15,11 @@ const Tempapp = () => {
     });
     const [search, setSearch] = useState(""); //112173060a86fb43967cc14bc0b6d40b
 
+    console.log(process.env)
     const fetchApi = React.useCallback(async () => {
         console.log('enter')
         try {
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=112173060a86fb43967cc14bc0b6d40b`);
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
             response.data && setData((prevState) => {
                 return {
                     ...prevState,
@@ -56,7 +57,6 @@ const Tempapp = () => {
         )
 
     }
-    console.log(data.err.message)
     return (
         <>
             <div className="main__div">
